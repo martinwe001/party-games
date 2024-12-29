3721a601712869e08bfa57c4133d750262c37c32
Martin Wangen-Eriksen <martin.wangen-eriksen@bredvid.no>
2024-12-29 15:56:29 +0100

    fix build

--- src/Pages/Jeopardy/JeopardyAdmin.tsx
+++ src/Pages/Jeopardy/JeopardyAdmin.tsx
@@ -1,9 +1,7 @@
 import { Flex, Stack, Text } from "@mantine/core";
 import { JeopardyForm } from "../../Components/Jeopardy/JeopardyForm";
-import { useJeopardy } from "../../hooks/useJeopardy";
 
 export function JeopardyAdmin() {
-  const { jeopardy } = useJeopardy();
   return (
     <Flex>
       <Stack bg={"white"}>
